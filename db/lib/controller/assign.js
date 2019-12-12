9e3719a1f431b6392e1a43071958d58959c142f2
jabin753 <jabin753@gmail.com>
2019-12-11 18:01:50 -0600

    [db] lint

--- db/lib/controller/assign.js
+++ db/lib/controller/assign.js
@@ -1,24 +1,24 @@
 'use strict'
 module.exports = function setupAssign (AssignModel) {
-    async function createOrUpdate (assign) {
-        const cond = {
-          where: {
-            uuid: assign.uuid
-          }
-        }
-        const existingAssign = await AssignModel.findOne(cond)
-        if (existingAssign) {
-          const updated = await AssignModel.update(assign, cond)
-          return updated ? AssignModel.findOne(cond) : existingAssign
-        }
-        const result = await AssignModel.create(assign)
-        return result.toJson()
+  async function createOrUpdate (assign) {
+    const cond = {
+      where: {
+        uuid: assign.uuid
       }
-      function findById (id) {
-        return AssignModel.findById(id)
-      }
-      return {
-        createOrUpdate,
-        findById
-      }
+    }
+    const existingAssign = await AssignModel.findOne(cond)
+    if (existingAssign) {
+      const updated = await AssignModel.update(assign, cond)
+      return updated ? AssignModel.findOne(cond) : existingAssign
+    }
+    const result = await AssignModel.create(assign)
+    return result.toJson()
+  }
+  function findById (id) {
+    return AssignModel.findById(id)
+  }
+  return {
+    createOrUpdate,
+    findById
+  }
 }
--- db/lib/controller/member.js
+++ db/lib/controller/member.js
@@ -1,24 +1,24 @@
 'use strict'
 module.exports = function setupMember (MemberModel) {
-    async function createOrUpdate (member) {
-        const cond = {
-          where: {
-            uuid: member.uuid
-          }
-        }
-        const existingMember = await MemberModel.findOne(cond)
-        if (existingMember) {
-          const updated = await MemberModel.update(member, cond)
-          return updated ? MemberModel.findOne(cond) : existingMember
-        }
-        const result = await MemberModel.create(member)
-        return result.toJson()
+  async function createOrUpdate (member) {
+    const cond = {
+      where: {
+        uuid: member.uuid
       }
-      function findById (id) {
-        return MemberModel.findById(id)
-      }
-      return {
-        createOrUpdate,
-        findById
-      }
+    }
+    const existingMember = await MemberModel.findOne(cond)
+    if (existingMember) {
+      const updated = await MemberModel.update(member, cond)
+      return updated ? MemberModel.findOne(cond) : existingMember
+    }
+    const result = await MemberModel.create(member)
+    return result.toJson()
+  }
+  function findById (id) {
+    return MemberModel.findById(id)
+  }
+  return {
+    createOrUpdate,
+    findById
+  }
 }
