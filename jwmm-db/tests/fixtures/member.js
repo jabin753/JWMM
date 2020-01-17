564c327e8c3b8ecea729635fcf508e93753dc22d
Joseph <jabin753@gmail.com>
2020-01-17 13:40:48 -0600

    [db] lint

--- jwmm-db/tests/fixtures/member.js
+++ jwmm-db/tests/fixtures/member.js
@@ -1,13 +1,13 @@
 const member = {
-    firstName: "John",
-      lastName: "Doe",
-      phone: "7777777777",
-      genre: "Male",
-      isServant: true,
-      isAssistant: false,
-      birthDate: "1970-07-07",
+  firstName: 'John',
+  lastName: 'Doe',
+  phone: '7777777777',
+  genre: 'Male',
+  isServant: true,
+  isAssistant: false,
+  birthDate: '1970-07-07'
 }
 
 module.exports = {
-    single: member
+  single: member
 }
