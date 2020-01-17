7e4436e1e2931be8f9abd9d8e129c91b9c49e2d8
Joseph <jabin753@gmail.com>
2020-01-17 16:50:36 -0600

    [api] lint

--- jwmm-api/config.js
+++ jwmm-api/config.js
@@ -4,15 +4,15 @@ const debug = require('debug')('jwmm:api:db')
 require('dotenv/config')
 
 module.exports = {
-    db: {
-        database: process.env.DB_NAME,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        logging: s => debug(s)
-    },
-    auth: {
-        secret: process.env.SECRET || 'jwmm'
-    }
+  db: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: s => debug(s)
+  },
+  auth: {
+    secret: process.env.SECRET || 'jwmm'
+  }
 }
--- jwmm-api/server.js
+++ jwmm-api/server.js
@@ -15,27 +15,27 @@ app.use('/api', api)
 
 // Error Handlers
 app.use((err, req, res, next) => {
-    debug(`Error: ${err.message}`)
+  debug(`Error: ${err.message}`)
 
-    if (err.message.match(/not found/)) {
-        return res.status(404).send({ error: err.message })
-    }
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
 
-    res.status(500).send({ error: err.message })
+  res.status(500).send({ error: err.message })
 })
 
 function handleFatalError (err) {
-    console.error(`${chalk.red('[fatal error]')} ${err.message}`)
-    console.error(err.stack)
-    process.exit(1)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
 }
 
-if(!module.parent) {
-    process.on('uncaughtException', handleFatalError)
-    process.on('unhandledRejection', handleFatalError)
+if (!module.parent) {
+  process.on('uncaughtException', handleFatalError)
+  process.on('unhandledRejection', handleFatalError)
 
-    server.listen(port, () => {
-        console.log(`${chalk.red('[jwmm-api]')} server listening on ${port}`)
-    })
+  server.listen(port, () => {
+    console.log(`${chalk.red('[jwmm-api]')} server listening on ${port}`)
+  })
 }
 module.exports = server
