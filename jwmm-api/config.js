d7529aa7a703616c0cdcbc5262bc64404898ec8f
jabin753 <jabin753@gmail.com>
2020-01-22 16:14:50 -0600

    [api] default credentials set

--- jwmm-api/config.js
+++ jwmm-api/config.js
@@ -5,10 +5,10 @@ require('dotenv/config')
 
 module.exports = {
   db: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
+    database: process.env.DB_NAME || 'jwmm',
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: s => debug(s)
   },
