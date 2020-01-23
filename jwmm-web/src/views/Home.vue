d3882c8590f10c66c3fd1535c8f1107bc785d5fc
Joseph <jabin753@gmail.com>
2020-01-23 14:17:41 -0600

    [web] vue project set

---
+++ jwmm-web/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import '@/assets/css/tailwind.css'
+
+export default {
+  name: 'home',
+  components: {}
+}
+</script>
