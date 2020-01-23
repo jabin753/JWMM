44ba42627fcad02d97fa421c52561ffb77f34bf6
Joseph <jabin753@gmail.com>
2020-01-23 14:34:17 -0600

    [web] JwNav component

--- jwmm-web/src/App.vue
+++ jwmm-web/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <jw-nav></jw-nav>
     <router-view />
   </div>
 </template>
+<script>
+import JwNav from '@/components/JwNav'
+export default {
+  components: { JwNav }
+}
+</script>
